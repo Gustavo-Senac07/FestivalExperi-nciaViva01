06de02faa244b190d072ebd346d4ef45d805fd11
Gustavo Dangelo Delabrida Rodrigues <gustavorodrigues252006@gmail.com>
2026-09-01 11:09:00 -0300

    model inscricao

---
+++ model/inscricao.php
@@ -0,0 +1,20 @@
+<?php
+
+class inscricao
+{
+    private $participante_id_participante;
+    private $atividades_id_atividades;
+    private $status;
+
+    public function __construct(
+        $participante_id_participante,
+        $atividades_id_atividades,
+    )
+    {
+        $this->participante_id_participante = $participante_id_participante;
+        $this->atividades_id_atividades = $atividades_id_atividades;
+    }
+
+    public function getParticipanteId() { return $this->participante_id_participante; }
+    public function getAtividadeId() { return $this->atividades_id_atividades; }
+}
